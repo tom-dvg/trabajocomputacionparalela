f7d936e474859d4820d0c9f8f59f5484f34f8606
Zeruen0 <pablovcr@outlook.cl>
2024-07-12 03:33:04 -0400

    Add files via upload

---
+++ PalDrive/build/CMakeFiles/mi_programa.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mi_programa.dir/main.cpp.o"
+  "CMakeFiles/mi_programa.dir/main.cpp.o.d"
+  "mi_programa"
+  "mi_programa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mi_programa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ PalDrive/build/CMakeFiles/mi_programa.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mi_programa.
+# This may be replaced when dependencies are built.
